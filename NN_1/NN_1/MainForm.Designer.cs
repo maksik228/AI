c4c2f79116b406d60a0b913eb8c79a5a0ad870b3
maksik228 <reactive94@gmail.com>
2019-01-14 21:23:45 +0300

    Add files via upload

---
+++ NN_1/NN_1/MainForm.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace NN_1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Grid_pictureBox = new System.Windows.Forms.PictureBox();
+            this.ApplySettings_button = new System.Windows.Forms.Button();
+            this.Teach_button = new System.Windows.Forms.Button();
+            this.IsTrue_checkBox = new System.Windows.Forms.CheckBox();
+            this.Recognize_button = new System.Windows.Forms.Button();
+            this.Recognize_textBox = new System.Windows.Forms.TextBox();
+            this.LearningSpeed_label = new System.Windows.Forms.Label();
+            this.LearningSpeed_numericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.Threshold_label = new System.Windows.Forms.Label();
+            this.Threshold_numericUpDown = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.Grid_pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LearningSpeed_numericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Threshold_numericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Grid_pictureBox
+            // 
+            this.Grid_pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Grid_pictureBox.BackColor = System.Drawing.Color.White;
+            this.Grid_pictureBox.Location = new System.Drawing.Point(29, 96);
+            this.Grid_pictureBox.Name = "Grid_pictureBox";
+            this.Grid_pictureBox.Size = new System.Drawing.Size(300, 300);
+            this.Grid_pictureBox.TabIndex = 0;
+            this.Grid_pictureBox.TabStop = false;
+            this.Grid_pictureBox.Click += new System.EventHandler(this.Grid_pictureBox_Click);
+            this.Grid_pictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.Grid_pictureBox_Paint);
+            this.Grid_pictureBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Grid_pictureBox_MouseClick);
+            // 
+            // ApplySettings_button
+            // 
+            this.ApplySettings_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ApplySettings_button.Location = new System.Drawing.Point(29, 41);
+            this.ApplySettings_button.Name = "ApplySettings_button";
+            this.ApplySettings_button.Size = new System.Drawing.Size(160, 29);
+            this.ApplySettings_button.TabIndex = 9;
+            this.ApplySettings_button.Text = "Применить";
+            this.ApplySettings_button.UseVisualStyleBackColor = true;
+            this.ApplySettings_button.Click += new System.EventHandler(this.ApplySettings_button_Click);
+            // 
+            // Teach_button
+            // 
+            this.Teach_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Teach_button.Location = new System.Drawing.Point(191, 402);
+            this.Teach_button.Name = "Teach_button";
+            this.Teach_button.Size = new System.Drawing.Size(81, 29);
+            this.Teach_button.TabIndex = 10;
+            this.Teach_button.Text = "Обучить";
+            this.Teach_button.UseVisualStyleBackColor = true;
+            this.Teach_button.Click += new System.EventHandler(this.Teach_button_Click);
+            // 
+            // IsTrue_checkBox
+            // 
+            this.IsTrue_checkBox.AutoSize = true;
+            this.IsTrue_checkBox.Checked = true;
+            this.IsTrue_checkBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.IsTrue_checkBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.IsTrue_checkBox.Location = new System.Drawing.Point(46, 407);
+            this.IsTrue_checkBox.Name = "IsTrue_checkBox";
+            this.IsTrue_checkBox.Size = new System.Drawing.Size(87, 24);
+            this.IsTrue_checkBox.TabIndex = 13;
+            this.IsTrue_checkBox.Text = "Правда";
+            this.IsTrue_checkBox.UseVisualStyleBackColor = true;
+            this.IsTrue_checkBox.CheckedChanged += new System.EventHandler(this.IsTrue_checkBox_CheckedChanged);
+            // 
+            // Recognize_button
+            // 
+            this.Recognize_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Recognize_button.Location = new System.Drawing.Point(358, 169);
+            this.Recognize_button.Name = "Recognize_button";
+            this.Recognize_button.Size = new System.Drawing.Size(124, 86);
+            this.Recognize_button.TabIndex = 13;
+            this.Recognize_button.Text = "Распознать";
+            this.Recognize_button.UseVisualStyleBackColor = true;
+            this.Recognize_button.Click += new System.EventHandler(this.Recognize_button_Click);
+            // 
+            // Recognize_textBox
+            // 
+            this.Recognize_textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Recognize_textBox.Location = new System.Drawing.Point(358, 285);
+            this.Recognize_textBox.Name = "Recognize_textBox";
+            this.Recognize_textBox.Size = new System.Drawing.Size(115, 26);
+            this.Recognize_textBox.TabIndex = 14;
+            // 
+            // LearningSpeed_label
+            // 
+            this.LearningSpeed_label.AutoSize = true;
+            this.LearningSpeed_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LearningSpeed_label.Location = new System.Drawing.Point(206, 9);
+            this.LearningSpeed_label.Name = "LearningSpeed_label";
+            this.LearningSpeed_label.Size = new System.Drawing.Size(85, 20);
+            this.LearningSpeed_label.TabIndex = 5;
+            this.LearningSpeed_label.Text = "Скорость:";
+            // 
+            // LearningSpeed_numericUpDown
+            // 
+            this.LearningSpeed_numericUpDown.DecimalPlaces = 2;
+            this.LearningSpeed_numericUpDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LearningSpeed_numericUpDown.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.LearningSpeed_numericUpDown.Location = new System.Drawing.Point(297, 7);
+            this.LearningSpeed_numericUpDown.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.LearningSpeed_numericUpDown.Name = "LearningSpeed_numericUpDown";
+            this.LearningSpeed_numericUpDown.Size = new System.Drawing.Size(73, 26);
+            this.LearningSpeed_numericUpDown.TabIndex = 6;
+            this.LearningSpeed_numericUpDown.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            65536});
+            // 
+            // Threshold_label
+            // 
+            this.Threshold_label.AutoSize = true;
+            this.Threshold_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Threshold_label.Location = new System.Drawing.Point(33, 9);
+            this.Threshold_label.Name = "Threshold_label";
+            this.Threshold_label.Size = new System.Drawing.Size(59, 20);
+            this.Threshold_label.TabIndex = 7;
+            this.Threshold_label.Text = "Порог:";
+            // 
+            // Threshold_numericUpDown
+            // 
+            this.Threshold_numericUpDown.DecimalPlaces = 2;
+            this.Threshold_numericUpDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Threshold_numericUpDown.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.Threshold_numericUpDown.Location = new System.Drawing.Point(116, 9);
+            this.Threshold_numericUpDown.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.Threshold_numericUpDown.Name = "Threshold_numericUpDown";
+            this.Threshold_numericUpDown.Size = new System.Drawing.Size(73, 26);
+            this.Threshold_numericUpDown.TabIndex = 8;
+            this.Threshold_numericUpDown.Value = new decimal(new int[] {
+            8,
+            0,
+            0,
+            65536});
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(537, 466);
+            this.Controls.Add(this.Threshold_numericUpDown);
+            this.Controls.Add(this.IsTrue_checkBox);
+            this.Controls.Add(this.Threshold_label);
+            this.Controls.Add(this.Recognize_textBox);
+            this.Controls.Add(this.LearningSpeed_label);
+            this.Controls.Add(this.LearningSpeed_numericUpDown);
+            this.Controls.Add(this.Teach_button);
+            this.Controls.Add(this.Recognize_button);
+            this.Controls.Add(this.ApplySettings_button);
+            this.Controls.Add(this.Grid_pictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "MainForm";
+            this.Text = "Распознование изображений с помощью персиптрона";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Grid_pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LearningSpeed_numericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Threshold_numericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Grid_pictureBox;
+        private System.Windows.Forms.Button ApplySettings_button;
+        private System.Windows.Forms.Button Teach_button;
+        private System.Windows.Forms.CheckBox IsTrue_checkBox;
+        private System.Windows.Forms.Button Recognize_button;
+        private System.Windows.Forms.TextBox Recognize_textBox;
+        private System.Windows.Forms.Label LearningSpeed_label;
+        private System.Windows.Forms.NumericUpDown LearningSpeed_numericUpDown;
+        private System.Windows.Forms.Label Threshold_label;
+        private System.Windows.Forms.NumericUpDown Threshold_numericUpDown;
+    }
+}
+
---
+++ NN_1/NN_1/MainForm.cs
@@ -0,0 +1,445 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace NN_1
+{
+    public partial class MainForm : Form
+    {
+        PaintingGrid PG;
+
+        Perceptron Pers;
+
+        List<LearningPicture> LearningSample;
+
+        //List<List<double>> Weights;
+        //List<> TrainingSample;
+        public MainForm()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Grid_pictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            PG.DrawGrid(e.Graphics,new Point(0, 0), Grid_pictureBox.Size, new Pen(Color.Black, 2));
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            //PG = new PaintingGrid((int)X_numericUpDown.Value, (int)Y_numericUpDown.Value);
+            PG = new PaintingGrid(5,5);
+            Grid_pictureBox.Refresh();
+        }
+
+        private void Grid_pictureBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            int Step_I = Grid_pictureBox.Size.Width / PG.RowCount;
+            int Step_J = Grid_pictureBox.Size.Height / PG.ColumnCount;
+            int Index_I = e.Y / Step_I;
+            int Index_J = e.X / Step_J;
+            if (PG.Matrix[Index_I][Index_J] == false)
+            {
+                PG.Matrix[Index_I][Index_J] = true;
+            }
+            else
+            {
+                PG.Matrix[Index_I][Index_J] = false;
+            }
+
+
+            Grid_pictureBox.Refresh();
+            Recognize_textBox.Clear();
+        }
+
+        //private void X_numericUpDown_ValueChanged(object sender, EventArgs e)
+        //{
+        //    PG = new PaintingGrid((int)X_numericUpDown.Value, (int)Y_numericUpDown.Value);
+        //    Grid_pictureBox.Refresh();
+        //}
+
+        //private void Y_numericUpDown_ValueChanged(object sender, EventArgs e)
+        //{
+        //    PG = new PaintingGrid((int)X_numericUpDown.Value, (int)Y_numericUpDown.Value);
+        //    Grid_pictureBox.Refresh();
+        //}
+
+        private void ApplySettings_button_Click(object sender, EventArgs e)
+        {
+            if (ApplySettings_button.Text == "Применить")
+            {
+               // Pers = new Perceptron((int)X_numericUpDown.Value, (int)Y_numericUpDown.Value, (double)LearningSpeed_numericUpDown.Value, (double)Threshold_numericUpDown.Value);
+                Pers = new Perceptron(5, 5, (double)LearningSpeed_numericUpDown.Value, (double)Threshold_numericUpDown.Value);
+               // Setting_groupBox.Enabled = false;
+                //Setting_groupBox.Enabled = true;
+                //Teach_groupBox.Enabled = true;
+                //Recognize_groupBox.Enabled = true;
+                ApplySettings_button.Text = "Настроить";
+            }
+            else
+            {
+                //Setting_groupBox.Enabled = true;
+                //Teach_groupBox.Enabled = false;
+                //Recognize_groupBox.Enabled = false;
+                ApplySettings_button.Text = "Применить";
+            }
+           
+        }
+
+        private void Teach_button_Click(object sender, EventArgs e)
+        {
+            bool IsLeant = Pers.Teach(PG.Matrix, this.IsTrue_checkBox.Checked);
+            if (this.LearningSample == null)
+                this.LearningSample = new List<LearningPicture>();
+
+            List<List<bool>> NewMatrix = new List<List<bool>>();
+            foreach (List<bool> row in PG.Matrix)
+            {
+                NewMatrix.Add(new List<bool>());
+                foreach (bool pix in row)
+                {
+                    NewMatrix.Last().Add(pix);
+                }
+            }
+
+            this.LearningSample.Add(new LearningPicture(NewMatrix, this.IsTrue_checkBox.Checked));
+            while (IsLeant == false)
+            {
+                IsLeant = true;
+                foreach (LearningPicture LP in this.LearningSample)
+                {
+                    //не обяз true
+                    if (Pers.Teach(LP.Picture, LP.Answer) == false)
+                    {
+                        IsLeant = false;
+                    }
+                }
+            }
+            PG.Clear();
+            Grid_pictureBox.Refresh();
+
+            //Pers.Teach(PG.Matrix, true);
+
+        }
+
+        private void Select_Learning_Sample_button_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OFD = new OpenFileDialog();
+            OFD.Filter = "*.txt|*.txt";
+            if (OFD.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.LearningSample = new List<LearningPicture>();
+                try
+                {
+                    StreamReader SR = new StreamReader(OFD.FileName);
+                    int PicturesCount = Convert.ToInt32(SR.ReadLine());
+                    int RowsCount = Convert.ToInt32(SR.ReadLine());
+                    int ColumnCount = Convert.ToInt32(SR.ReadLine());
+
+                    for (int p = 0; p < PicturesCount; p++)
+                    {
+                        int ss = Convert.ToInt32(SR.ReadLine());
+                        bool ans = Convert.ToBoolean(ss);
+                        List<List<bool>> Pic = new List<List<bool>>();
+                        //LearningSample.Add(new List<List<bool>>());
+                        for (int r = 0; r < RowsCount; r++)
+                        {
+                            Pic.Add(new List<bool> ());
+                            //LearningSample[p].Add(new List<bool>());
+                            for (int c = 0; c < ColumnCount; c++)
+                            {
+                                int next = Convert.ToInt32(SR.Read());
+                                //string s = Convert.ToString(SR.Read());
+                                switch((char)next)
+                                {
+                                    case '1':
+                                        //LearningSample[p][r].Add(true);
+                                        Pic[r].Add(true);
+                                        break;
+                                    case '0':
+                                        //LearningSample[p][r].Add(false);
+                                        Pic[r].Add(false);
+                                        break;
+                                    default:
+                                        throw new Exception();
+                                }
+                            }
+                            SR.ReadLine();
+                        }
+                        LearningPicture LP = new LearningPicture(Pic, ans);
+                        this.LearningSample.Add(LP);
+                        
+                        SR.ReadLine();
+                    }
+
+                    //обучение
+                    bool IsLeant = false;
+                    while (IsLeant == false)
+                    {
+                        IsLeant = true;
+                        foreach (LearningPicture LP in this.LearningSample)
+                        {
+                            //не обяз true
+                            if (Pers.Teach(LP.Picture, LP.Answer) == false)
+                            {
+                                IsLeant = false;
+                            }
+                        }
+                    }
+                    SR.Close();
+
+                }
+                catch { }
+            }
+            
+        }
+
+        private void Save_Learning_Sample_button_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog SFD = new SaveFileDialog();
+            SFD.Filter = "*.txt|*.txt";
+            if (SFD.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                try
+                {
+                    StreamWriter SW = new StreamWriter(SFD.FileName);
+                    SW.WriteLine(this.LearningSample.Count);
+                    SW.WriteLine(this.PG.RowCount);
+                    SW.WriteLine(this.PG.ColumnCount);
+
+                    for (int p = 0; p < this.LearningSample.Count; p++)
+                    {
+                        SW.WriteLine(Convert.ToInt32(this.LearningSample[p].Answer));
+                        for (int r = 0; r < this.PG.RowCount; r++)
+                        {
+                            for (int c = 0; c < this.PG.ColumnCount; c++)
+                            {
+                                SW.Write(Convert.ToInt32(this.LearningSample[p].Picture[r][c]));
+                            }
+                            SW.WriteLine(";");
+                        }
+                    SW.WriteLine();
+                    }
+                    SW.Close();
+                }
+                catch { }
+            }
+        }
+
+        private void Recognize_button_Click(object sender, EventArgs e)
+        {
+            if (this.Pers.CheckPicture(PG.Matrix))
+            {
+                Recognize_textBox.Text = "Правда";
+            }
+            else
+            {
+                Recognize_textBox.Text = "Ложь";
+            }
+        }
+
+        private void Grid_pictureBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void IsTrue_checkBox_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    class PaintingGrid
+    {
+        public List<List<bool>> Matrix = new List<List<bool>>();
+        public int ColumnCount;
+        public int RowCount;
+        //Point Location;
+        //Size Size;
+        //Graphics G;
+        //Pen
+
+        public void DrawGrid(Graphics g, Point location, Size size, Pen p)
+        {
+            int StepY = size.Height / this.RowCount;
+            int StepX = size.Width / this.ColumnCount;
+            for (int i = location.Y; i <= size.Height; i += StepY)
+            {
+                g.DrawLine(p, new Point(location.X, i), new Point(location.X + size.Width, i));
+            }
+            for (int j = location.X; j <= size.Width; j += StepX)
+            {
+                g.DrawLine(p, new Point(j, location.Y), new Point(j, location.Y + size.Height));
+            }
+
+            for (int i = 0; i < this.RowCount; i++)
+                for (int j = 0; j < this.ColumnCount; j++)
+                {
+                    if (Matrix[i][j] == true)
+                    {
+                        g.FillRectangle(Brushes.Black, new Rectangle(j * StepX, i * StepY, StepX, StepY));
+                    }
+                }
+        }
+        public PaintingGrid(int cols, int rows)
+        {
+            this.Matrix = new List<List<bool>>();
+            this.ColumnCount = cols;
+            this.RowCount = rows;
+
+            for (int i = 0; i < RowCount; i++)
+            {
+                Matrix.Add(new List<bool>());
+                for (int j = 0; j < ColumnCount; j++)
+                {
+                    Matrix[i].Add(false);
+                }
+            }
+        }
+
+        public void Clear()
+        {
+            for (int i = 0; i < RowCount; i++)
+            {
+                for (int j = 0; j < ColumnCount; j++)
+                {
+                    Matrix[i][j] = false;
+                }
+            }
+        }
+    }
+
+    class Perceptron
+    {
+        int ColumnCount;
+        int RowCount;
+        List<List<double>> Weights;
+        double LearningSpeed;
+        double Threshold;
+
+        public Perceptron (int cols, int rows, double ls,double th)
+        {
+            this.ColumnCount = cols;
+            this.RowCount = rows;
+            this.LearningSpeed = ls;
+            this.Threshold = th;
+
+            this.Weights = new List<List<double>>();
+            for (int i = 0; i < RowCount; i++)
+            {
+                Weights.Add(new List<double>());
+                for (int j = 0; j < ColumnCount; j++)
+                {
+                    Weights[i].Add(1);
+                }
+            }
+            //Weights[0][0] = 1;
+            this.NormalizeWeights();
+        }
+
+        double PictureSum(List<List<bool>> Pic)
+        {
+            double Sum = 0;
+            for (int i = 0; i < RowCount; i++)
+            {
+                for (int j = 0; j < ColumnCount; j++)
+                {
+                    if (Pic[i][j])
+                        Sum += Weights[i][j];
+                }
+            }
+            return Sum;
+        }
+        void NormalizeWeights()
+        {
+            //находим мин
+            //double min = 1;
+            //foreach (List<double> row in this.Weights)
+            //    foreach (double w in row)
+            //    {
+            //        if (w < min)
+            //        {
+            //            min = w;
+            //        }
+            //    }
+            //вычитаем мин и суммируем
+            double Sum = 0;
+            foreach (List<double> row in this.Weights)
+                foreach (double w in row)
+                {
+                    Sum += w;
+                }
+
+            for (int i = 0; i < this.RowCount; i++)
+                for (int j = 0; j < this.ColumnCount; j++)
+                {
+                    this.Weights[i][j] = (this.Weights[i][j]) / Sum;
+                    //this.Weights[i][j] = this.Weights[i][j] / (this.RowCount * this.ColumnCount);
+                }
+        }
+
+        public bool CheckPicture(List<List<bool>> Pic)
+        {
+            double Sum = this.PictureSum(Pic);
+            if (Sum >= this.Threshold)
+                return true;
+            else
+                return false;
+        }
+        public bool Teach(List<List<bool>> Pic, bool result) //true если уже обучен
+        {
+            if (this.CheckPicture(Pic) == result)
+                return true;
+            while (this.CheckPicture(Pic) != result) //если ответ неверный
+            {
+                double err;
+                double inc;
+
+                if (this.CheckPicture(Pic) == false) //если должно быть false, то вычитаем
+                {
+                    err = 1 - this.PictureSum(Pic);
+                    inc = err * this.LearningSpeed;
+                }
+                else
+                {
+                    err = this.PictureSum(Pic);
+                    inc = err * this.LearningSpeed;
+                    inc = inc * (-1);
+                }
+                for (int i = 0; i < Pic.Count; i++)
+                {
+                    for (int j = 0; j < Pic[i].Count; j++)
+                    {
+                        if (Pic[i][j] == true)
+                        {
+                            this.Weights[i][j] += inc;
+                            if (Weights[i][j] < 0)
+                                Weights[i][j] = 0;
+                        }
+                    }
+                }
+                this.NormalizeWeights();
+            }
+            return
+                false;
+            
+        }
+    }
+
+    class LearningPicture
+    {
+        public List<List<bool>> Picture;
+        public bool Answer;
+        public LearningPicture(List<List<bool>> picture, bool answer)
+        {
+            this.Answer = answer;
+            this.Picture = picture;
+        }
+    }
+}
